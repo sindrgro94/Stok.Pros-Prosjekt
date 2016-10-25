6d9af38b616de6a8eb0c9bc5fbcd82e2d6e737a1
Halvorsg <halvor.snersrud@gmail.com>
2016-10-25 16:50:55 +0200

    Bedre og bedre

--- task3a.m
+++ task3a.m
@@ -1,16 +1,29 @@
 function [P,Prob]= task3a()
-    l = 3;
-    %The probability for more than 200 claims before March 1st
-    P = 0;
-    t = 59;%days
-P = 1-poisscdf(200,l*t);
+lambda = 3;
+%The probability for more than 200 claims before March 1st
+
+t = 59;%days
+P = 1-poisscdf(200,lambda*t);
 %Funksjonen under er definert som P(x < X) normalfordeling 
 Z = 1-normcdf(200,177,sqrt(177)); % gir en approksimasjon på hva den kommer
 % til å bli
 
+
 %Expected waiting time Sn = SUM(Tn)
 %E[Tn] = 1/l
-S = 10*1/l;
+S = 10*1/lambda;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Simulering
+m = zeros(1,1000);
+for i = 1:1000
+n = poissrnd(lambda*10);
+s = rand(1,n)*10;
+s = sort(s);
+m(i) = s(10);
+end
+SimS = sum(m)/length(m);
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -19,7 +32,7 @@ S = 10*1/l;
 y = zeros(1,10000);
 cnt = 0;
 for i  = 1:10000
-    n = poissrnd(l*t);
+    n = poissrnd(lambda*t);
     if n>200
         cnt = cnt+1;
     end
