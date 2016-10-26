8a6eebcf3bd31ebc11274132324ab8ebbb709f33
Halvorsg <halvor.snersrud@gmail.com>
2016-10-26 14:56:48 +0200

    Nb(t)

--- task3b.m
+++ task3b.m
@@ -2,11 +2,14 @@ function ret= task3b(t)
 %Want to use thinning
 lambda = 3;
 lambda_Var = @(time) 2+cos(time*pi/182.5);
-n = poissrnd(lambda*t);
-s = rand(1,n)*t;
-s = sort(s);
-y = zeros(1,5);
-cnt = 0;
+tot = 0;
+hold on
+for j = 1:1000
+    n = poissrnd(lambda*t);
+    s = rand(1,n)*t;
+    s = sort(s);
+    y = zeros(1,50);
+    cnt = 0;
 for i = 1:n
     X = rand;
     if X < lambda_Var(s(i))/lambda
@@ -14,9 +17,8 @@ for i = 1:n
         y(cnt) = s(i);
     end
 end
+tot = length(y)+tot;
+plot(y,[1:length(y)])
+end
 ret = length(y);
-hold on
-plot(y,[1:length(y)],'ob')
-plot(s,[1:length(s)],'xr')
-
 end
